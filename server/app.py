ac82d201076ed848b5dc88a7ee1918b93625ea25
Dev Aggarwal <devxpy@gmail.com>
2026-03-07 15:28:11 -0800

    Align HF Spaces runtime to port 7860

--- server/app.py
+++ server/app.py
@@ -80,7 +80,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="0.0.0.0")
-    parser.add_argument("--port", type=int, default=8000)
+    parser.add_argument("--port", type=int, default=7860)
     args = parser.parse_args()
 
     uvicorn.run(app, host=args.host, port=args.port)
